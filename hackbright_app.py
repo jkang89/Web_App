a7ed41cbf3bba014bae6ceb22b46393b0dae4474
chriszf <chriszf@gmail.com>
2012-10-17 15:47:38 -0700

    Adding initial app

---
+++ hackbright_app.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+DB = None
+
+def get_student_by_github(github):
+    query = """SELECT first_name, last_name, github FROM Students WHERE github = ?"""
+    DB.execute(query, (github,))
+    row = DB.fetchone()
+    print """\
+Student: %s %s
+Github account: %s"""%(row[0], row[1], row[2])
+
+def connect_to_db():
+    global DB
+    DB = sqlite3.connect("hackbright.db").cursor()
+
+def main():
+    connect_to_db()
+    command = None
+    while command != "quit":
+        input_string = raw_input("HBA Database> ")
+        tokens = input_string.split()
+        command = tokens[0]
+        args = tokens[1:]
+
+        if command == "student":
+            get_student_by_github(*args) 
+
+if __name__ == "__main__":
+    main()
